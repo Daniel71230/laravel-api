e1a3a59378cc7c623643c3c762898cda72504c04
Daniel71230 <133103434+Daniel71230@users.noreply.github.com>
2023-11-01 20:06:17 +0200

    Update locals.tf

---
+++ locals.tf
@@ -0,0 +1,18 @@
+locals {
+  bucket_name = "cc-tf-demo"
+  table_name  = "ccTfDemo"
+
+  ecr_repo_name = "laravel-app-ecr-repo"
+
+  laravel_app_cluster_name        = "laravel-web-app"
+  availability_zones           = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  laravel_app_task_family         = "laravel-web-app"
+  container_port               = 3000
+  laravel_app_task_name           = "laravel-web-app"
+  ecs_task_execution_role_name = "laravel-web-app-execution-role"
+
+  app_load_balancer_name = "laravel-app-alb"
+  target_group_name              = "laravel-alb-tg"
+
+  laravel_app_service_name = "laravel-app-service"
+}
