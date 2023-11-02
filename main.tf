8eea92b6155013138881e632993f24397dc87205
Daniel71230 <133103434+Daniel71230@users.noreply.github.com>
2023-11-02 16:47:10 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,134 +1,32 @@
-resource "aws_ecs_cluster" "laravel_app_cluster" {
-  name = var.laravel_app_cluster_name
-}
+terraform {
+  required_version = "~> 1.5"
 
-resource "aws_default_vpc" "default_vpc" {}
-
-resource "aws_default_subnet" "default_subnet_a" {
-  availability_zone = var.availability_zones[0]
-}
-
-resource "aws_default_subnet" "default_subnet_b" {
-  availability_zone = var.availability_zones[1]
-}
-
-resource "aws_default_subnet" "default_subnet_c" {
-  availability_zone = var.availability_zones[2]
-}
-
-resource "aws_ecs_task_definition" "laravel_app_task" {
-  family                   = var.laravel_app_task_family
-  container_definitions    = <<DEFINITION
-  [
-    {
-      "name": "${var.laravel_app_task_name}",
-      "image": "${var.ecr_repo_url}",
-      "essential": true,
-      "portMappings": [
-        {
-          "containerPort": ${var.container_port},
-          "hostPort": ${var.container_port}
-        }
-      ],
-      "memory": 512,
-      "cpu": 256
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
     }
-  ]
-  DEFINITION
-  requires_compatibilities = ["FARGATE"]
-  network_mode             = "awsvpc"
-  memory                   = 512
-  cpu                      = 256
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
-}
-
-resource "aws_iam_role" "ecs_task_execution_role" {
-  name               = var.ecs_task_execution_role_name
-  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
-  role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
-resource "aws_alb" "app_load_balancer" {
-  name               = var.app_load_balancer_name
-  load_balancer_type = "application"
-  subnets = [
-    "${aws_default_subnet.default_subnet_a.id}",
-    "${aws_default_subnet.default_subnet_b.id}",
-    "${aws_default_subnet.default_subnet_c.id}"
-  ]
-  security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
-}
-
-resource "aws_security_group" "load_balancer_security_group" {
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-resource "aws_lb_target_group" "target_group" {
-  name        = var.target_group_name
-  port        = var.container_port
-  protocol    = "HTTP"
-  target_type = "ip"
-  vpc_id      = aws_default_vpc.default_vpc.id
+module "ecrRepo" {
+  source = "./modules/ecr"
+
+  ecr_repo_name = local.ecr_repo_name
 }
 
-resource "aws_lb_listener" "listener" {
-  load_balancer_arn = aws_alb.app_load_balancer.arn
-  port              = "80"
-  protocol          = "HTTP"
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.target_group.arn
-  }
-}
+module "ecsCluster" {
+  source = "./modules/ecs"
 
-resource "aws_ecs_service" "laravel_app_service" {
-  name            = var.laravel_app_service_name
-  cluster         = aws_ecs_cluster.laravel_app_cluster.id
-  task_definition = aws_ecs_task_definition.laravel_app_task.arn
-  launch_type     = "FARGATE"
-  desired_count   = 1
+  laravel_app_cluster_name = local.laravel_app_cluster_name
+  availability_zones    = local.availability_zones
 
-  load_balancer {
-    target_group_arn = aws_lb_target_group.target_group.arn
-    container_name   = aws_ecs_task_definition.laravel_app_task.family
-    container_port   = var.container_port
-  }
-
-  network_configuration {
-    subnets          = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}", "${aws_default_subnet.default_subnet_c.id}"]
-    assign_public_ip = true
-    security_groups  = ["${aws_security_group.service_security_group.id}"]
-  }
-}
-
-resource "aws_security_group" "service_security_group" {
-  ingress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  laravel_app_task_family         = local.laravel_app_task_family
+  ecr_repo_url                 = module.ecrRepo.repository_url
+  container_port               = local.container_port
+  laravel_app_task_name        = local.laravel_app_task_name
+  ecs_task_execution_role_name = local.ecs_task_execution_role_name
+  app_load_balancer_name = local.app_load_balancer_name
+  target_group_name              = local.target_group_name
+  laravel_app_service_name          = local.laravel_app_service_name
 }
