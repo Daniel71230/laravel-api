f8e1902e879414b4d81c2e6310fa74c66758cd6f
Daniel <prsnetok@gmail.com>
2023-11-29 21:50:16 +0200

    policies added

--- modules/ecs/data.tf
+++ modules/ecs/data.tf
@@ -8,11 +8,3 @@ data "aws_iam_policy_document" "assume_role_policy" {
     }
   }
 }
-
-data "aws_iam_policy_document" "ecs_task_execution_policy" {
-  statement {
-    actions   = ["secretsmanager:GetSecretValue"]
-    effect    = "Allow"
-    resources = ["*"]
-  }
-}
