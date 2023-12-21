7f99902d01ba1e4c05edf55c2a9dfef45289d6b3
Daniel71230 <133103434+Daniel71230@users.noreply.github.com>
2023-12-21 15:45:03 +0200

    Update entrypoint.sh

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -15,9 +15,9 @@ php artisan optimize
 php artisan clear
 php artisan optimize:clear
 php artisan cache:clear
-#php artisan migrate
+php artisan migrate
 
-#php artisan db:seed
+php artisan db:seed
 
 # Files ownership change.
 chown -R www-data .
